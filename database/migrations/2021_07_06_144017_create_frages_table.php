06b002b0e8e13c7d94ffcfefd55c8cdf4a42efff
j0ha <46874251+j0ha@users.noreply.github.com>
2021-07-06 19:28:33 +0200

    added livewire

---
+++ database/migrations/2021_07_06_144017_create_frages_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFragesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('frages', function (Blueprint $table) {
+            $table->id();
+            $table->text("frage");
+            $table->string('autor')->default('Anonym');
+            $table->boolean('beantwortet')->default(false);
+            $table->boolean('live')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('frages');
+    }
+}
