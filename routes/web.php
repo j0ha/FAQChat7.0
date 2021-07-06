7a82bbbce4a18f2eddc6ed4022dee0f7628566b6
j0ha <46874251+j0ha@users.noreply.github.com>
2021-07-06 19:27:28 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/fragestellen', 'FragenstellenController@index');
+Route::post('/fragestellen', 'FragenstellenController@fragestellen')->name('frage.stellen');
+
+Route::get('/fragen', function (){
+    return view('crud');
+});
